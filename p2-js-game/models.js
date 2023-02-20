d8d85f8db0305b704ab0f54dee9a0f1e3cfebff0
jagger85 <andresfonsortega@yahoo.es>
2023-02-20 19:23:40 +0800

    User input testing | simon interface improved

--- p2-js-game/models.js
+++ p2-js-game/models.js
@@ -2,16 +2,21 @@ class myBox extends HTMLDivElement{
     constructor(){
         super()
         this.style.cssText = 'border:1px solid black; width:100px;height:100px;'
+        this.classList.add('box')
+        let state = 'inactive'
     }
    
-    activate(seconds){
+    activate(){
         this.style.border = '10px solid black'
-        setTimeout(()=>{
-        this.style.border = '1px solid black'
-        },seconds*1000)
+        this.state = 'active';
     }
-   
+
+    deactivate(){
+        this.style.border = '1px solid black'
+        this.state = 'inactive' 
+    }
 }
+
 customElements.define('custom-box',myBox,{extends:'div'})
 
 export const box1 = document.createElement('div',{is:'custom-box'})
@@ -23,6 +28,13 @@ box1.style.backgroundColor = 'red'
 box2.style.backgroundColor = 'blue'
 box3.style.backgroundColor = 'yellow'
 box4.style.backgroundColor = 'green'
+
+box1.setAttribute('id', '1');
+box2.setAttribute('id', '2');
+box3.setAttribute('id', '3');
+box4.setAttribute('id', '4');
+
+
 
 export const start = document.createElement('button')
 export const reset = document.createElement('button')
--- p2-js-game/script.js
+++ p2-js-game/script.js
@@ -1,14 +1,40 @@
 import {sound} from './sound.js'
 import {init} from './view.js'
-import {box1,box2,box3,box4} from './models.js'
+import {box1,box2,box3,box4,start,reset} from './models.js'
 
 const sequence = [];
+const userSequence = [];
 const velocity = 0.5;
 const boxes = [box1,box2,box3,box4]
 
-init()
-startSequence(createSequence(),2)
 
+/*
+/   Initialize the interface
+*/
+init()
+
+/*
+/   Event listeners
+*/
+start.addEventListener('click',()=>{
+    startSequence(createSequence(),2)
+})
+
+reset.addEventListener('click',()=>{
+    console.log(userSequence)
+})
+
+let input = document.querySelector('#main').addEventListener('click',(e)=>{
+    if(e.target.className == 'box'){
+        console.log('box clicked')
+        userSequence.push(+e.target.id)
+        return +e.target.id
+    }
+})
+
+/*
+/   Game logic
+*/
 
 function startGame(){
     box2.activate(2)
@@ -19,21 +45,44 @@ function createSequence(steps){
 }
 
 function startSequence(sequence,velocity){
+    let n = 0;
+    step(sequence,velocity)
 
-    // sequence.forEach((e,i)=>{
-    //     setTimeout(()=>{
-    //         boxes[i].activate(velocity)
-    //     },velocity*1000)
-    // })
+    function step(sequence,velocity){
 
-
+    if(sequence[n]==undefined)finishGame();
+    else{
+        boxes[sequence[n]].activate()
+    setTimeout(()=>{
+        boxes[sequence[n]].deactivate()
+        n++
+        setTimeout(()=>{
+            step(sequence,velocity)
+        },100)
+        
+    },velocity*1000)
+    }}
 }
 
-function readUserInput(sequence){
+function readUserInput(sequence,i){
+
+    let userInput = new Promise((resolve,reject)=>{
+        if(input == 1){
+            resolve('ok')
+        }else{
+            reject('bad')
+        }
+    })
+
+    userInput.then((message) =>{
+        console.log(message)
+    }).catch((message)=>{
+        console.log(message)
+    })
 
 }
 
 function finishGame(){
-
+    console.log('Game Finished')
 }
 
